4f3d6f2e79a006404d90b50a0bdda565e4d32be6
nazim1971 <md.nazimuddinaj@gmail.com>
2024-03-13 16:41:08 +0600

    added blog json file

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-2xl font-semibold text-center '>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
